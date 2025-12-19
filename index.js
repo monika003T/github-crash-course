2ddb42256d8c5cf390e474d3927d209b6d1eb4fc
monika003T <monikathakur2062@gmail.com>
2025-12-19 19:15:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const age=24;
+const name="Mukku"
+const dob="03/11/2003"
+
+function sayname(){
+    console.log('my name is Mukku');
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+function sayyourname(){
+    console.log("charu")
+}
